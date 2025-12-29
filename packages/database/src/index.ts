9b5b67a97939564ffc07ad32370206d6d214c2bc
Mohit Mourya <mohitmouryaa143@gmail.com>
2025-12-29 11:53:47 +0530

    Update Prisma client output path and imports
    Changed Prisma client output directory to src/generated/client in schema.prisma and updated related imports in index.ts. Adjusted .gitignore to exclude the new generated client directory. Updated next-env.d.ts to reference the correct routes types path.

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from ".prisma/client";
+import { PrismaClient } from "./generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -6,4 +6,4 @@ export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export * from ".prisma/client";
+export * from "./generated/client";
